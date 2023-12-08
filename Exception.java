00d75eff323051db6d77cef6a24e25bdd5e5aca8
Yuito Arakaki <e235741@ie.u-ryukyu.ac.jp>
2023-12-08 10:20:20 +0900

    third commit

--- Exception.java
+++ Exception.java
@@ -1,11 +1,7 @@
 public class Exception {
     public static void main(String[] args){
-        String str = null;
-        try{System.out.println(str.length());
-        } catch (NullPointerException e){
-            System.out.println("NullPointerExceptionが発生しました。");
-            System.out.println(e.getMessage());
-        }
+        String str = "壱百満";
+        int value = Integer.parseInt(str);
     }
     
 }
